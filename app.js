1d281b4a85eb25b6c97cc5bbc5e32697e345a014
Krizpia <sales@krizpia.com>
2026-07-04 15:30:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -88,49 +88,6 @@ function renderChecklist() {
         <button type="button" class="na" onclick="selectAnswer(${number}, 'na', this)">N/A</button>
       </div>
 
-      <div class="action-box" id="action-${number}">
-
-    <textarea id="comment-${number}" placeholder="Finding / Comment"></textarea>
-
-    <label class="photoLabel">📷 Evidence Photo</label>
-
-    <input
-        type="file"
-        id="photo-${number}"
-        accept="image/*"
-        capture="environment"
-        onchange="previewPhoto(${number}, this)"
-    >
-
-    <img
-        id="preview-${number}"
-        class="previewImage"
-        style="display:none;"
-    >
-
-    <textarea
-        id="actionText-${number}"
-        placeholder="Corrective Action"
-    ></textarea>
-
-    <input
-        type="text"
-        id="responsible-${number}"
-        placeholder="Responsible Person"
-    >
-
-    <input
-        type="date"
-        id="deadline-${number}"
-    >
-
-    <input
-        type="text"
-        id="verification-${number}"
-        placeholder="Verification"
-    >
-
-</div>
     `;
 
     checklist.appendChild(div);
